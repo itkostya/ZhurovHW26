3cfe4fffb9846d83c2530ba426d1162b15bb980a
kostya <kostya@dnepr-it2.dc.snackprod.com>
2016-08-12 17:30:31 +0300

    тест 2

---
+++ ZhurovHW26/src/main/java/com/mycompany/zhurovhw26/git_test/GitTest2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.zhurovhw26.git_test;
+
+/**
+ *
+ * @author kostya
+ */
+public class GitTest2 {
+    
+}
